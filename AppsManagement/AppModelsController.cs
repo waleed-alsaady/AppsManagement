3a56063f4aa43e9ed34389450e74ac3e9a0533d6
wal88 <waleedalsaady1@gmail.com>
2020-09-19 21:46:13 +0400

    Add MVC CRUD views

---
+++ AppsManagement/AppModelsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppsManagement.Data;
+using AppsManagement.Models;
+
+namespace AppsManagement
+{
+    public class AppModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AppModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Apps.ToListAsync());
+        }
+
+        // GET: AppModels/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appModel = await _context.Apps
+                .FirstOrDefaultAsync(m => m.AppId == id);
+            if (appModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(appModel);
+        }
+
+        // GET: AppModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AppId,Name,OrganizationId,ChromeWebOrigin")] AppModel appModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                _context.Add(appModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appModel);
+        }
+
+        // GET: AppModels/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appModel = await _context.Apps.FindAsync(id);
+            if (appModel == null)
+            {
+                return NotFound();
+            }
+            return View(appModel);
+        }
+
+        // POST: AppModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("AppId,Name,OrganizationId,ChromeWebOrigin")] AppModel appModel)
+        {
+            if (id != appModel.AppId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppModelExists(appModel.AppId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appModel);
+        }
+
+        // GET: AppModels/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appModel = await _context.Apps
+                .FirstOrDefaultAsync(m => m.AppId == id);
+            if (appModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(appModel);
+        }
+
+        // POST: AppModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var appModel = await _context.Apps.FindAsync(id);
+            _context.Apps.Remove(appModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppModelExists(string id)
+        {
+            return _context.Apps.Any(e => e.AppId == id);
+        }
+    }
+}
---
+++ AppsManagement/Data/Migrations/20200919165054_AddAppModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppsManagement.Data.Migrations
+{
+    public partial class AddAppModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apps",
+                columns: table => new
+                {
+                    AppId = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    OrganizationId = table.Column<string>(nullable: true),
+                    ChromeWebOrigin = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apps", x => x.AppId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apps");
+        }
+    }
+}
